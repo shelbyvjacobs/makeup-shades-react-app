cefbd474daa07a44dad39dfdb25f2aa9de9383df
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-11-17 14:44:46 -0500

    added update form and delete buttons

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -10,18 +10,29 @@ export default function Body(props) {
             <div className="Body">
                 <div className="Tile" style={tileStyle} key={shades.id}>
                     <Link to="/hex/:hex" className="HexCode">{shades.hex}</Link>
+                    <button type="button" className="DeleteButton">X</button>
                 </div>
             </div>
         );
     });
     return  (
     <div>
-        <form className="CreateForm">
-            Add a Shade: <br/><input type="text" placeholder="Brand"></input><br/>
-            <input type="text" placeholder="Product"></input><br/>
-            <input type="text" placeholder="Hex Code"></input><br/>
-            <input type="submit" value="submit"></input>
-        </form>
+        <div className="Forms">
+            <form className="CreateForm">
+                Add a Shade: <br/>
+                <input type="text" placeholder="Brand"></input><br/>
+                <input type="text" placeholder="Product"></input><br/>
+                <input type="text" placeholder="Hex Code"></input><br/>
+                <input type="submit" value="Submit"></input>
+            </form>
+            <form className="UpdateForm">
+                Update a Shade <br/>
+                <input type="text" placeholder="Brand"></input><br/>
+                <input type="text" placeholder="Product"></input><br/>
+                <input type="text" placeholder="Hex Code"></input><br/>
+                <input type="submit" value="Submit"></input>
+            </form>
+        </div>
         <div>{tileList}</div>
     </div>
     )
