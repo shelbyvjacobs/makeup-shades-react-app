402d4249b1fdbbd08c4334021b732cb16a916e7c
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-11-17 20:11:40 -0500

    cleaning up code

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { Link } from "react-router-dom"
 
 export default function Brand(props) {
-    // let brandList = props.shades.map(brands => {
         return (
             <div className="Body">
                 <h2>Brands</h2>
-                {/* <Link to="/group/:group">{brands.group}</Link> */}
                 <Link to="/brand/:brandShort" className="link">brand here</Link>
             </div>
         );
-    // })
-    // return <div>{brandList}</div>
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -5,7 +5,6 @@ export default function Country(props) {
     return (
         <div className="Body">
             <h2>Countries</h2>
-            {/* <Link to="/group/:group">{brands.group}</Link> */}
             <Link to="/group/:group" className="link">countries here</Link>
         </div>
     );
